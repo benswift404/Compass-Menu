8e3c2939d106572bc85952868bb4802a9c93b1cb
Ben Swift <swiftmac234@gmail.com>
2021-02-10 13:08:55 -0500

    Initial Commit

---
+++ Compass Menu/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Compass Menu
+//
+//  Created by Ben Swift on 2/10/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
